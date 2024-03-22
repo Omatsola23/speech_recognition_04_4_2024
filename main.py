993a863e041cb525a60edcbcfac0f6a1af00c858
omatsolaeyewu-edero <omatsolajennings@yahoo.com>
2024-03-22 14:44:43 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+import speech_recognition as sr
+import streamlit as st
+import parselmouth
+import sounddevice as sd
+import numpy as np
+from parselmouth.praat import call
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def transcribe_speech():
+    # Initialize recognizer class
+    r = sr.Recognizer()
+    # Reading Microphone as source
+    with sr.Microphone() as source:
+        st.info("Speak now...")
+        # listen for speech and store in audio_text variable
+        audio_text = r.listen(source)
+        st.info("Transcribing...")
 
+        try:
+            # using Google Speech Recognition
+            text = r.recognize_google(audio_text)
+            return text
+        except:
+            return "Sorry, I did not get that."
+def transcribe_voice():
+    def record_audio(duration=5, fs=44100):
+        st.info("Speak now...")
+        audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='float32')
+        sd.wait()  # Wait until recording is finished
+        return audio_data.flatten(), fs
+        st.info("Transcribing...")
+    def transcribe_audio(audio_data, sampling_frequency):
+            snd = parselmouth.Sound(audio_data, sampling_frequency=sampling_frequency)
+            waveform = snd.values.T[0]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+            # Perform speech recognition
+            speech = call(waveform, sampling_frequency, "To TextGrid (silences)", "praat")
+            return speech
+def main():
+    st.title("Speech Recognition App")
+    api_options = ["Google Speech Recognition", "Parselmouth"]
+    selected_api=st.selectbox("Select speech recognition API:", api_options)
+    if selected_api == "Google Speech Recognition":
+        st.button("Start Recording")
+        text = transcribe_speech()
+        st.write("Transcription: ", text)
+    else:
+        st.button("Start Recording")
+        speech = transcribe_voice()
+        st.write("Transcribed text:", speech)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
