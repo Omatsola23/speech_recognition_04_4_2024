6e4179b6664a3d04ba2a210e12cf0e0272986ad5
omatsolaeyewu-edero <omatsolajennings@yahoo.com>
2024-03-25 16:20:07 +0100

    update

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import speech_recognition as sr
 import streamlit as st
-import parselmouth
-import sounddevice as sd
-import numpy as np
-from parselmouth.praat import call
-
 def transcribe_speech():
     # Initialize recognizer class
     r = sr.Recognizer()
@@ -22,31 +17,41 @@ def transcribe_speech():
         except:
             return "Sorry, I did not get that."
 def transcribe_voice():
-    def record_audio(duration=5, fs=44100):
+    # Initialize recognizer class
+    c = sr.Recognizer()
+    # Reading Microphone as source
+    with sr.Microphone() as source:
         st.info("Speak now...")
-        audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='float32')
-        sd.wait()  # Wait until recording is finished
-        return audio_data.flatten(), fs
+        # listen for speech and store in audio_text variable
+        audio_text = c.listen(source)
         st.info("Transcribing...")
-    def transcribe_audio(audio_data, sampling_frequency):
-            snd = parselmouth.Sound(audio_data, sampling_frequency=sampling_frequency)
-            waveform = snd.values.T[0]
 
-            # Perform speech recognition
-            speech = call(waveform, sampling_frequency, "To TextGrid (silences)", "praat")
+        try:
+            # using Google Speech Recognition
+            speech = c.recognize_google(audio_text)
             return speech
+        except:
+            return "Sorry, I did not get that."
 def main():
     st.title("Speech Recognition App")
-    api_options = ["Google Speech Recognition", "Parselmouth"]
+    api_options = ["Google Speech Recognition", "Vosk"]
     selected_api=st.selectbox("Select speech recognition API:", api_options)
     if selected_api == "Google Speech Recognition":
         st.button("Start Recording")
         text = transcribe_speech()
         st.write("Transcription: ", text)
+
+        file_name = "transcribed_text.txt"
+        with open(file_name, "w") as file:
+            file.write(text)
     else:
         st.button("Start Recording")
         speech = transcribe_voice()
         st.write("Transcribed text:", speech)
+
+        file_name = "transcribed_text.txt"
+        with open(file_name, "w") as file:
+            file.write(speech)
 
 if __name__ == "__main__":
     main()
